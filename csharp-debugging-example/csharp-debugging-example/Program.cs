d0fb0b433f1e7f6078d0914cabeba31023ba50e1
DESKTOP-30N4GA5\Djuro <djurovic.milos1998@gmail.com>
2020-10-19 15:38:47 +0200

    Zamenjeno malim slovima

--- csharp-debugging-example/csharp-debugging-example/Program.cs
+++ csharp-debugging-example/csharp-debugging-example/Program.cs
@@ -9,9 +9,9 @@ namespace csharp_debugging_example
         {
             List<Student> listaStudenata = new LinkedList<Student>()
             {
-            new Student("1", "Milan Novakovic", new Department("RI"));
-            new Student("2", "Nikola Milanovic", new Department("IT"));
-            new Student("3", "Milos Milosevic", new Department("IT"));
+            new Student("1", "Milan Novakovic", new Department("ri"));
+            new Student("2", "Nikola Milanovic", new Department("it"));
+            new Student("3", "Milos Milosevic", new Department("it"));
 
             for (int i = 0; i < listaStudenata.Count; i++)
                 Console.WriteLine(listaStudenata[1].ToString);
