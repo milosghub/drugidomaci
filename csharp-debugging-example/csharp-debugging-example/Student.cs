8a2019c6e6a8c61b932e02898423c48d6172feb0
DESKTOP-30N4GA5\Djuro <djurovic.milos1998@gmail.com>
2020-10-19 15:27:48 +0200

    Dodati konstruktor i toString metoda

--- csharp-debugging-example/csharp-debugging-example/Student.cs
+++ csharp-debugging-example/csharp-debugging-example/Student.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 
 namespace csharp_debugging_example
@@ -9,5 +10,17 @@ namespace csharp_debugging_example
         string Name;
         string IdNumber;
         Department Department;
+
+        public Student (string name, string Idnumber, Department department)
+        {
+            this.Name = name;
+            this.IdNumber = Idnumber;
+            this.Department = Department;
+
+            Public string toString()
+            {
+                return "<" +this.Name+ ">" + "-" + "<" + this.IdNumber+ ">" + "-" + "<" +this.Department+ ">";
+            }
+        }
     }
 }
