5779bb5f08d334a11f7e1f85b20c46f5cc6b7d03
DESKTOP-30N4GA5\Djuro <djurovic.milos1998@gmail.com>
2020-10-19 15:07:47 +0200

    Kreiran konstruktor

---
+++ csharp-debugging-example/csharp-debugging-example/Department.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace csharp_debugging_example
+{
+    class Department
+    {
+        string Title;
+        string Code;
+        public Department(string code)
+        {
+            if (code.Equals("IT"))
+                Title = "Informacione tehnologije";
+            else if (code.Equals("RI")
+                Title = "Racunarska tehnika";
+                
+        }
+    }
+}
