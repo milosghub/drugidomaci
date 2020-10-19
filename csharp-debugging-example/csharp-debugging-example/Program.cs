94ef631f70c518dfe0ee68e41125da3bb8287baa
DESKTOP-30N4GA5\Djuro <djurovic.milos1998@gmail.com>
2020-10-19 14:52:17 +0200

    Kreirana konzolna aplikacija

---
+++ csharp-debugging-example/csharp-debugging-example/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace csharp_debugging_example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
