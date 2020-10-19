117023c2105068a4119b56d5bb26dd14fe342865
DESKTOP-30N4GA5\Djuro <djurovic.milos1998@gmail.com>
2020-10-19 15:10:16 +0200

    Kreirana klasa Student

---
+++ csharp-debugging-example/csharp-debugging-example/Student.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace csharp_debugging_example
+{
+    class Student
+    {
+        string Name;
+        string IdNumber;
+        Department Department;
+    }
+}
